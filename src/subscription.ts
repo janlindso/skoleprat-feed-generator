1a86dfcd252f59e2ce9c7f6ad7fcc884788b6c70
janlindso <jan.frode.lindso@gmail.com>
2023-08-05 03:29:57 +0200

    Update subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -19,11 +19,11 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     const postsToDelete = ops.posts.deletes.map((del) => del.uri)
     const postsToCreate = ops.posts.creates
       .filter((create) => {
-        // only alf-related posts
-        return create.record.text.toLowerCase().includes('alf')
+        // only skole-related posts
+        return create.record.text.toLowerCase().includes('#skole')
       })
       .map((create) => {
-        // map alf-related posts to a db row
+        // map skole-related posts to a db row
         return {
           uri: create.uri,
           cid: create.cid,
